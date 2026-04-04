1fbad30666645dd1d74cdc9e8aed3628a8b45894
Tushar Solanki <tusharbharat700@gmail.com>
2026-04-04 18:07:03 +0530

    fix swagger HTTPS

--- src/main/java/com/smartspend/smartspend_api/config/SwaggerConfig.java
+++ src/main/java/com/smartspend/smartspend_api/config/SwaggerConfig.java
@@ -4,10 +4,22 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.util.List;
 
 @Configuration
 @OpenAPIDefinition(info = @Info(title = "SmartSpend API", version = "1.0", description = "AI-Powered Personal Finance API"))
 @SecurityScheme(name = "bearerAuth", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://smartspend-api-production.up.railway.app"),
+                        new Server().url("http://localhost:8082")));
+    }
 }
